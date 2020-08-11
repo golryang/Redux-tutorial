1c269d6625f68faffc72ac0dff2719c5cdedca54
golryang <41885614+golryang@users.noreply.github.com>
2020-08-12 04:33:52 +0900

    Update App.js

--- App.js
+++ App.js
@@ -7,11 +7,14 @@ import CounterContainer from './containers/CounterContainer';
 import TodosContainer from './containers/TodosContainer';
 // 커밋
 const App = () => {
-  return (
-    <div className="TodoTemplate">
-      <TodosContainer />
-    </div>
-  );
+  return <CounterContainer />;
+  /*
+  할 일 목록 컴포넌트로 교체
+
+  (<div className="TodoTemplate">
+    <TodosContainer /> 
+  </div>);
+*/
 };
 
 export default App;
